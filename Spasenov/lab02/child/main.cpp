93e56a3484665224c4e71e6f068124ea1f841a07
sefin123 <spasenov2004@mail.ru>
2024-04-04 00:12:49 +0300

    Add lab02

---
+++ Spasenov/lab02/child/main.cpp
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <locale.h>
+
+extern char **environ;
+
+#define VAR_NUM 6
+
+char** getShortVariables(const char* fname) {
+    FILE *f = NULL;
+    if ((f = fopen(fname, "r")) != NULL) {                             
+        char **result = (char **)calloc(VAR_NUM, sizeof(char *));
+        for (int i = 0; i < VAR_NUM; i++) {
+            result[i] = (char*)calloc(80, sizeof(char));             
+            fgets(result[i], 80, f);                                        // Чтение всех строк.
+            result[i][strlen(result[i])-1] = '\0';
+        }
+        fclose(f);
+        return result;
+    }
+    printf("Error while open file.\n");
+    return NULL;
+}
+
+int includeString(const char* str1, const char* str2) {
+    for(size_t i = 0; i<strlen(str2); i++)
+        if(str1[i]!=str2[i])
+            return 0;
+    return 1;
+}
+
+char* findEnvpVariable(char* envp[], const char* variable) {
+    int i = 0;
+    while(envp[i]) {
+        if(includeString(envp[i], variable))
+            break;
+        i++;
+    }
+    return envp[i];
+}
+
+int main(int argc, char* argv[], char* envp[]) {
+
+    printf("Process name: %s\nProcess pid: %d\nProcess ppdid: %d\n", argv[0], (int) getpid(), (int) getppid());
+
+    char **variables = getShortVariables(argv[1]);                             // Получаем список необходимых переменных среды из файла.
+    if (variables != NULL) {
+        switch (argv[2][0]) {
+            case '+':
+                for (int i = 0; i < VAR_NUM; i++)                              // Получаем переменную среды системной функцией.
+                    printf("%s=%s\n", variables[i], getenv(variables[i]));
+                break;
+            case '*':
+                for (int i = 0; i < VAR_NUM; i++)                              // Получаем переменную среды из аргументов.
+                    printf("%s\n", findEnvpVariable(envp, variables[i]));
+                break;
+            case '&':
+                for (int i = 0; i < VAR_NUM; i++)                              // Получаем переменную среды из внешней переменной.
+                    printf("%s\n", findEnvpVariable(environ, variables[i]));
+                break;
+        }
+        for (int i = 0; i < VAR_NUM; i++)
+            free(variables[i]);
+    }
+    free(variables);
+    return 0;
+}
---
+++ Spasenov/lab02/parent/main.cpp
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <locale.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+extern char **environ;
+
+int includeString(const char* str1, const char* str2) {
+    for(size_t i = 0; i<strlen(str2); i++)
+        if(str1[i]!=str2[i])
+            return 0;
+    return 1;
+}
+
+char* scanEnvp(char* envp[]) {
+    int i = 0;
+    while(envp[i]) {
+        if(includeString(envp[i], "CHILD_PATH"))                           // Обход всех параметров и проверка на совпадение.
+            break;
+        i++;
+    }
+    if(envp[i]==NULL)
+        return NULL;
+    char* result = (char*)calloc(strlen(envp[i]) - 10, sizeof(char));
+    for(size_t j = 0; j<strlen(envp[i]); j++)                                 
+        result[j] = envp[i][j+11];
+    result[strlen(envp[i])-11] = '\0';
+    return result;
+}
+
+int compare(const void *a, const void *b) {
+    return strcoll(*(const char **)a, *(const char **)b);
+}
+
+int main(int argc, char* argv[], char* envp[]) {
+
+    setlocale(LC_COLLATE, "C");                                                      // Установить локаль.
+
+    int i = 0;
+    while (envp[i++]);                                                               // Узнать число параметров среды.
+    qsort(envp, i-1, sizeof(char*), compare);                                        // Сортировка.
+    i = 0;
+    while(envp[i]) {
+        printf("%s\n", envp[i++]);
+    }
+
+    char *childName = (char*)calloc(9, sizeof(char));
+    strcpy(childName, "child_00");
+
+    while(1) {
+        char key = getchar();                                      
+        while (getchar() != '\n')
+            continue;
+        if (key == 'q')
+            break;
+        else if (key == '+' || key == '*' || key == '&') {
+            pid_t chpid = fork();                                                    // Новый процесс.
+            if (chpid == 0) {   
+                char *childDir = NULL;
+                switch (key) {                                                       // Получаем путь к программе сhild в соответствии с нажатой клавишей.
+                    case '+':
+                        childDir = getenv("CHILD_PATH");
+                        break;
+                    case '*':
+                        childDir = scanEnvp(envp);
+                        break;
+                    case '&':
+                        childDir = scanEnvp(environ);
+                        break;
+                }
+                if (childDir) {
+                    char *newArgv[] = {childName, argv[1], &key, NULL};
+                    execve(childDir, newArgv, envp);                                // Вызов программы.
+                } else {
+                    printf("Error occured, path not find.\n");
+                }
+            } else if (chpid == -1) {
+                printf("Error occured, error code - %d\n", errno);
+                exit(errno);
+            }
+        } else
+            printf("Unknown key. Press '+', '*', '&' or q.\n");
+        waitpid(-1, NULL, WNOHANG);                                                 // Ожидание завершения процесса.
+    }
+
+    free(childName);
+
+    return 0;
+}
